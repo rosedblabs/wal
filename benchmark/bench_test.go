4315b483284e99ea5be359e44e555e8e370b6b6d
roseduan <roseduan7@163.com>
2023-07-29 19:26:09 +0800

    add wal benchmark

---
+++ benchmark/bench_test.go
@@ -0,0 +1,52 @@
+package benchmark
+
+import (
+	"github.com/rosedblabs/wal"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"os"
+	"testing"
+)
+
+var walFile *wal.WAL
+
+func init() {
+	dir, _ := os.MkdirTemp("", "wal-benchmark-test")
+	opts := wal.Options{
+		DirPath:        dir,
+		SegmentFileExt: ".SEG",
+		SegmentSize:    32 * 1024 * 1024,
+	}
+	var err error
+	walFile, err = wal.Open(opts)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func BenchmarkWAL_Write(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := walFile.Write([]byte("Hello World"))
+		assert.Nil(b, err)
+	}
+}
+
+func BenchmarkWAL_Read(b *testing.B) {
+	var positions []*wal.ChunkPosition
+	for i := 0; i < 1000000; i++ {
+		pos, err := walFile.Write([]byte("Hello World"))
+		assert.Nil(b, err)
+		positions = append(positions, pos)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := walFile.Read(positions[rand.Intn(len(positions))])
+		assert.Nil(b, err)
+	}
+}
